1c3a91706b5e5e98bc897a4cebd630439fc93b4f
ChrisGilliam12 <chrisgilliam123@gmail.com>
2020-01-23 13:51:37 -0600

    moved to a new repository to fix heroku

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('CheeseBurger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BarbequeBaconBurger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('VeggieBurger', FALSE);
